8e61f06687126c8f02c221f499c0dd1a7a85ecbc
Kevin Lam <klam@Kevins-Macbook-Pro.local>
2019-05-14 21:33:16 -0700

    Revises how Pokemon API is called

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
-let button = document.querySelector('#submit');
+let pokeButton = document.querySelector('#pokeButton');
+let randomUser = document.querySelector('#randomUser');
+
 
 let numberInput = document.querySelector('#numberInput');
 numberInput.addEventListener('input', getFactAjax);
@@ -21,28 +23,27 @@ function getFactAjax() {
 	xhr.send();
 }
 
-	submit.addEventListener('click', () => {
-		let number = numberInput.value;
+pokeButton.addEventListener('click', () => {
+	let number = numberInput.value;
 
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${number}`);
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${number}`);
 
-		xhr.onload = function() {
-			if (this.status === 200 && number !== '') {
-				let pokeObj = JSON.parse(this.responseText);
-				let pokeName = pokeObj.name[0].toUpperCase().concat(pokeObj.name.slice(1));
-				let theMoves = ''
-				let moves = pokeObj.moves.map(function(move) {
-					theMoves+= `<li>${move.move.name[0].toUpperCase().concat(move.move.name.slice(1))}</li>`
-				});
-				fact.style.display = 'block';
-				console.log('hy',pokeObj.sprites.front_default);
-				factText.innerHTML = `<p><strong>Name</strong>: ${pokeName}<br />`;
-				factText.innerHTML += `<img src="${pokeObj.sprites.front_default}" alt="${pokeName}" />`;
-				factText.innerHTML += `<p><strong>Moves</strong>: <br /> <ul>${theMoves}</ul>`;
-			} else {
-				console.log('That ain\'t right.')
-			}
+	xhr.onload = function() {
+		if (this.status === 200 && number !== '') {
+			let pokeObj = JSON.parse(this.responseText);
+			let pokeName = pokeObj.name[0].toUpperCase().concat(pokeObj.name.slice(1));
+			let theMoves = ''
+			let moves = pokeObj.moves.map(function(move) {
+				theMoves+= `<li>${move.move.name[0].toUpperCase().concat(move.move.name.slice(1))}</li>`
+			});
+			fact.style.display = 'block';
+			factText.innerHTML = `<p><strong>Name</strong>: ${pokeName}<br />`;
+			factText.innerHTML += `<img src="${pokeObj.sprites.front_default}" alt="${pokeName}" />`;
+			factText.innerHTML += `<p><strong>Moves</strong>: <br /> <ul>${theMoves}</ul>`;
+		} else {
+			console.log('That ain\'t right.')
 		}
-		xhr.send();
-	});
+	}
+	xhr.send();
+});
